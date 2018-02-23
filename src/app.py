ccb1e3d730d00710a55a5793c13fbd9d7e6cac1a
abdullahsumbal <abdullahsumbal@gmail.com>
2018-02-23 03:50:25 -0800

    wrl points rendering in application window

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from src.interface import MainWindow
+from src.interface.render import QTorsoViewer, WRLTest
+
 
 class AppWindow(QDialog):
     def __init__(self):
@@ -10,6 +12,13 @@ class AppWindow(QDialog):
         self.window = QMainWindow()
         self.ui = MainWindow.Ui_MainWindow()
         self.ui.setupUi(self.window)
+
+        # Set up VTK render in UI
+
+        # Render Sphere in the window
+        #QTorsoViewer(self.ui.render_frame)
+        # Render wrl files
+        WRLTest(self.ui.render_frame)
         self.window.show()
 
 app = QApplication(sys.argv)
--- src/interface/render.py
+++ src/interface/render.py
@@ -1,66 +1,94 @@
-from PyQt5 import QtWidgets
-from MainWindow import Ui_MainWindow
-import sys
 import vtk
-from QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-
-
-class ApplicationWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(ApplicationWindow, self).__init__()
-        self.vtk_widget = None
-        self.ui = None
-        self.setup()
-
-    def setup(self):
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.vtk_widget = QTorsoViewer(self.ui.render_frame)
-
-  # def initialize(self):
-        # self.vtk_widget.start()
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 
 class QTorsoViewer(QtWidgets.QFrame):
     def __init__(self, parent):
         super(QTorsoViewer,self).__init__(parent)
 
-        # VTK part
-        # create the widget
-        widget = QVTKRenderWindowInteractor(self)
-        widget.Initialize()
-        widget.Start()
-      
- 
-        ren = vtk.vtkRenderer()
-        widget.GetRenderWindow().AddRenderer(ren)
+        self.frame = Qt.QFrame()
+        self.vl = Qt.QVBoxLayout()
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.vl.addWidget(self.vtkWidget)
 
-        cone = vtk.vtkConeSource()
-        cone.SetResolution(8)
+        self.ren = vtk.vtkRenderer()
+        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
 
-        coneMapper = vtk.vtkPolyDataMapper()
-        coneMapper.SetInputConnection(cone.GetOutputPort())
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(5.0)
 
-        coneActor = vtk.vtkActor()
-        coneActor.SetMapper(coneMapper)
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
 
-        ren.AddActor(coneActor)
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        self.ren.AddActor(actor)
+
+        self.ren.ResetCamera()
+
+        self.setLayout(self.vl)
+        # self.setCentralWidget(self.frame)
+
+        self.show()
+        self.iren.Initialize()
+        self.iren.Start()
 
 
-        self.interactor = widget
+class WRLTest(QtWidgets.QFrame):
+    def __init__(self, parent):
+        super(WRLTest,self).__init__(parent)
+        self.frame = Qt.QFrame()
+        self.vl = Qt.QVBoxLayout()
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.vl.addWidget(self.vtkWidget)
 
-    # def start(self):
-    #     self.interactor.Initialize()
-    #     self.interactor.Start()
+        # Render
+        self.ren = vtk.vtkRenderer()
+        # Render Window
+        self.renWin = self.vtkWidget.GetRenderWindow()
+        # Add Render to Render Window
+        self.renWin.AddRenderer(self.ren)
+        # Interactor
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
 
+        # VRML importer
+        importer = vtk.vtkVRMLImporter()
+        # Set the Render Window for the importer
+        importer.SetRenderWindow(self.renWin)
+        # WRL file to upload
+        importer.SetFileName("/media/sumbal/C814-5638/2353729.wrl")
+        # Read data
+        importer.Read()
+        # config Screen
+        importer.GetRenderer().SetBackground(0.1, 0.2, 0.4)
+        importer.GetRenderWindow().SetSize(300, 300)
 
-        
+        # the importer created the renderer
+        renCollection = self.renWin.GetRenderers()
+        renCollection.InitTraversal()
+        ren = renCollection.GetNextItem()
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    application = ApplicationWindow()
-    application.show()
-    sys.exit(app.exec_())
+        # change view up to +z
+        #
+        ren.GetActiveCamera().SetPosition(-3.25303, 3.46205, 3.15906)
+        ren.GetActiveCamera().SetFocalPoint(0, 0, 0)
+        ren.GetActiveCamera().SetViewUp(0.564063, 0.825024, -0.0341876)
 
-if __name__ == "__main__":
-    main()
+        # let the renderer compute good position and focal point
+        #
+        ren.ResetCamera()
+        ren.GetActiveCamera().Dolly(1.75)
+        self.ren.ResetCameraClippingRange()
+
+        # Add layout to the frame
+        self.setLayout(self.vl)
+        self.show()
+        self.iren.Initialize()
+        self.iren.Start()
